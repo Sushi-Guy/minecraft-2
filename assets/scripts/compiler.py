23ff9fb25bdd6925059a403996af98f7b7c0ae66
Sushi-Guy <sushiredstone@gmail.com>
2026-03-14 18:57:11 -0700

    included nakama

--- assets/scripts/compiler.py
+++ assets/scripts/compiler.py
@@ -1,15 +1,16 @@
 import subprocess
+import shutil
 from xml.etree.ElementInclude import include
 
 file_name = "app"
 include_dir = "dependencies/include"
 lib_dir = "dependencies/lib"
-lib = "-lglfw3dll -lglew32s -lopengl32 -lgdi32"
+lib = "-lglfw3dll -lglew32s -lopengl32 -lgdi32 -lnakama-sdk"
 preprocessor = "-DGLEW_STATIC"
 
 def main():
     # include paths for generic project and imgui
-    includes = f"-I {include_dir} -I dependencies/imgui -I dependencies/imgui/backends"
+    includes = f"-I {include_dir} -I dependencies/imgui -I dependencies/imgui/backends -I dependencies/nakama-sdk/win-x64-Debug/include"
     
     # imgui source files we need to compile
     imgui_src = "dependencies/imgui/imgui.cpp dependencies/imgui/imgui_draw.cpp dependencies/imgui/imgui_tables.cpp dependencies/imgui/imgui_widgets.cpp dependencies/imgui/backends/imgui_impl_glfw.cpp dependencies/imgui/backends/imgui_impl_opengl2.cpp"
@@ -18,7 +19,8 @@ def main():
     subprocess.call(f"g++ -c assets/scripts/main.cpp {imgui_src} {includes}")
     
     # link all object files together
-    subprocess.call(f"g++ *.o -o {file_name} -L {lib_dir} {lib}")
+    subprocess.call(f"g++ *.o -o {file_name} -L {lib_dir} -L dependencies/nakama-sdk/win-x64-Debug/lib {lib}")
+    shutil.copy("dependencies/nakama-sdk/win-x64-Debug/lib/nakama-sdk.dll", ".")
 
 if "__main__" == __name__:
     main()
